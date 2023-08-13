0a23e4aea00d947dfb49f96d9a5b130976f277c1
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-13 15:44:15 +0600

    coding with JS

---
+++ index.js
@@ -0,0 +1,29 @@
+document.write("Hello World");
+
+console.log("Practicing javaScript");
+alert("Welcome to JS");
+
+document.getElementById("text").innerHTML = "JS is being used in both Front End and Back End Devolopment";
+
+var a = 20;
+var b = 10;
+
+var c = a+b;
+document.write("<br>"+c);
+
+var x = prompt("enter a number <br>");
+document.write("<br>"+x);
+
+var a = 20;
+b=a;
+
+a=50;
+
+document.write("<br>"+a);
+document.write("<br>"+b);
+
+var myValue =0, myvalue = 30;
+
+document.write("<br>"+myValue+" , "+myvalue);
+
+const a1 = 2;
