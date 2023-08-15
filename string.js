9c658185c1f7a0358c1c2cdc9fc6dbaf9ec1b3cf
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-15 16:32:43 +0600

    more practice on string

--- string.js
+++ string.js
@@ -23,7 +23,27 @@ document.write(x[0]);
 document.write(x[6]);
 
 // some methods
-var x = " Hello World ";
+var x = "Hello Worlds";
 document.write("<br>");
-document.write( x.slice(0,8));
+document.write( x.slice(0,9));
 x.slice();
+
+document.write("<br>");
+document.write( x.slice(-6,-1));
+document.write("<br>");
+document.write( x.substr(3,2));
+
+document.write("<br>");
+document.write(x = x.replace("Hello","Hi"));
+document.write("<br>");
+document.write( x);
+
+document.write("<br>");
+document.write(s = s.toUpperCase());
+
+document.write("<br>");
+document.write( s = s.toLowerCase());
+
+var new1 = "   NK Tanvir   ";
+document.write("<br>");
+document.write( new1.trim()); //it removes space from start, end side
