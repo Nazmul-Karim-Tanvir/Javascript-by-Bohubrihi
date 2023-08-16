d311962e2a2f041e77aa9b464045d993480b5c89
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-16 17:46:03 +0600

    object parcticing

---
+++ object.js
@@ -0,0 +1,28 @@
+//creating a object
+student = {name:"rahim",age:25,hometown:"Dhaka"};
+//finding a specific value 
+document.write(student.name);
+document.write("<br>");
+document.write(student["name"]);
+document.write("<br>");
+document.write(student.age);
+document.write("<br>");
+document.write(student.hometown);
+document.write("<br>");
+
+//making a object empty
+student = {};
+
+//adding value in object
+student.name = "Imran";
+document.write(student.name);
+document.write("<br>");
+
+student["name2"] = "hossain";
+document.write(student.name2);
+document.write("<br>");
+
+//deleting value from a object
+delete student.name2;
+document.write(student.name2);
+document.write("<br>");
