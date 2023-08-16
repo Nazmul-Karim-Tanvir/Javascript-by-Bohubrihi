a08abdc3fc94757f012af40a23cde6dadb1fcaf9
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-16 13:51:29 +0600

    boolean practice

---
+++ boolean.js
@@ -0,0 +1,26 @@
+document.write(10>9);
+document.write("<br>");
+document.write("Hello" == "Hello");
+document.write("<br>");
+document.write("Hello" == "World");
+
+var x = "Hello";
+var y = Boolean(x);
+
+document.write("<br>");
+document.write(x);
+document.write(y); //return true as value ir present 
+
+x =null;
+document.write("<br>");
+document.write(x);
+document.write(Boolean(x)); //return false for null value 
+x =0;
+document.write("<br>");
+document.write(x);
+document.write(Boolean(x)); //return false for 0 value
+
+var m;
+document.write("<br>");
+document.write(m);
+document.write(Boolean(m));// return false for empty/undefined  value 
