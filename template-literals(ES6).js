9589e52b896aa0f0bf114fd0d5cf5cd38a7c3fc1
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-17 12:13:07 +0600

    template literals practice done

---
+++ template-literals(ES6).js
@@ -0,0 +1,17 @@
+//Template literals (ES6)
+// Backtick ``
+
+let name = "Rahim";
+let age = "38";
+let dob = "21 June, 1983";
+
+document.write(`his name is ${name}
+<br> His age is ${age}
+<br> Date of Birth ${dob}`);
+
+let a = 34;
+let b = 67;
+
+console.log(`${a}+${b} = ${a+b}`);
+document.write("<br>"+`${a}+${b} = ${a+b}`);
+
