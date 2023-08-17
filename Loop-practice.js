cfbf1c96283690c1b65b1fd31ccacfdf69cb1aac
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-17 22:18:56 +0600

    for loop practice

---
+++ Loop-practice.js
@@ -0,0 +1,45 @@
+// while loop 
+// print 1 to 10
+var i=1;
+
+while(i<=10){
+    document.write(i+" ");
+    i++;
+}
+
+document.write("<br>");
+
+//sum 1 to 10
+i=1;
+var sum =0;
+while(i<=10){
+    sum= sum+i;
+    i++;
+}
+document.write("Sum is: "+sum);
+document.write("<br>");
+
+//product 1 to 10
+
+i=1;
+var product =1;
+while(i<=10){
+    product= product*i;
+    i++;
+}
+document.write("Product is: "+product);
+document.write("<br>");
+
+//for loop practice
+for(var k = 1; k<=10; k++){
+    document.write(k+" ");
+}
+
+document.write("<br>");
+
+//printing in reverse order using for loop
+for(var k = 10; k>=1; k--){
+    document.write(k+" ");
+}
+
+document.write("<br>");
