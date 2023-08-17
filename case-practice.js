cf3ba3f44ab98483bb8ae60eeba7dc3f9f5745b7
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-17 14:53:37 +0600

    case practice done

---
+++ case-practice.js
@@ -0,0 +1,23 @@
+
+var text = prompt("enter any letter from a to f");
+
+switch (text) {
+    case "a":
+        document.write("option 1 selected");
+        break;
+    case "b":
+        document.write("option 2 selected");
+        break;
+    case "c":
+        document.write("option 3 selected");
+        break;
+    case "d":
+        document.write("option 4 selected");
+        break;
+    case "e":
+        document.write("option 5 selected");
+        break;
+    case "f":
+        document.write("option 6 selected");
+        break;
+}
