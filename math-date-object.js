c704f27cb0bc8f2f0bb00197be35c3ce7ea38a05
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-20 21:48:43 +0600

    function date practice

--- math-date-object.js
+++ math-date-object.js
@@ -20,3 +20,35 @@ val = Math.random()*5;
 
 // Date Object
 let val1;
+
+val1 = new Date();
+val1 = new Date('8-20-2023 9:22:31');
+val1 = new Date('8/20/2023 9:22:31:1');
+document.write(val1.getMonth()); // month shows 0 to 11
+document.write("<br>");
+document.write(val1.getDate());
+document.write("<br>");
+document.write(val1.getDay());
+document.write("<br>");
+document.write(val1.getFullYear());
+document.write("<br>");
+document.write(val1.getHours());
+document.write("<br>");
+document.write(val1.getMinutes());
+document.write("<br>");
+document.write(val1.getSeconds());
+document.write("<br>");
+document.write(val1.getUTCMilliseconds());
+document.write("<br>");
+document.write(val1.getTime());// amount of time past since Jan 1st 1970
+
+let birthday = new Date('10/19/2014');
+
+birthday.setMonth(8);// 0 to 11
+birthday.setDate(14);
+birthday.setFullYear(1998);
+
+document.write("<br>");
+document.write(birthday);
+
+
