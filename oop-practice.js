8d7e0831334e756f6bf4a93e3031900c6ef10bcf
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-21 16:53:21 +0600

    class practice done

---
+++ oop-practice.js
@@ -0,0 +1,36 @@
+//class ES6
+// class is a template for creating objects
+
+class Person{
+    constructor(fname, lname, birthday){
+        this.firstname = fname;
+        this.lastname = lname;
+        this.dob = birthday;
+    }
+
+    calculateAge(){
+        let birthday = new Date(this.dob);
+        let diff = Date.now() - birthday.getTime();
+        let ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear()-1970);
+    }
+
+    fullname(){
+        return this.firstname+" "+this.lastname;
+    }
+
+}
+
+let person1 = new Person("Fazle","Rahet","9-14-1998");
+
+
+document.write(person1.firstname);
+document.write("<br>");
+document.write(person1.lastname);
+document.write("<br>");
+document.write(person1.dob);
+document.write("<br>");
+document.write(person1.calculateAge());
+document.write("<br>");
+document.write(person1.fullname());
+document.write("<br>");
