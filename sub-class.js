110b5a512cd9a63a83340d982028ee44b09434a7
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-22 15:02:19 +0600

    oop practice done will start dom today

--- sub-class.js
+++ sub-class.js
@@ -10,6 +10,9 @@ class Person {
     greeting(){
         return `Hello ${this.firstname} ${this.lastname} `;
     }
+    static test(){ // moved to Person class
+        return `HI `;
+    }
 }
 
 class Customer extends Person {
@@ -20,10 +23,30 @@ class Customer extends Person {
     }
 
     fullname(){
-        return "Nazmul karim tanvir";
+        return `${this.firstname} ${this.lastname}`;
     }
 }
 
-let Customer1 = new Customer("Fazle", "Rahat","Jashim","fahim");
+let Customer1 = new Customer("Fazle", "Rahat","01888383932","New");
 
 document.write(Customer1.firstname);
+document.write("<br>");
+document.write(Customer1.lastname);
+document.write("<br>");
+document.write(Customer1.phone);
+document.write("<br>");
+document.write(Customer1.membership);
+document.write("<br>");
+
+document.write(Customer1.fullname());
+document.write("<br>");
+
+document.write(Customer1.greeting());
+document.write("<br>");
+
+let Person1 = new Person();
+//document.write(Person1.fullname()); // parent can not access child class methods 
+
+//document.write(Person1.test());// can not call static function from object
+document.write(Person.test());// can call static function from class only
+document.write("<br>");
