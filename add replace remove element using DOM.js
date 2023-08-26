0452f50e8ff80f7dc1eb6780f976d4b8e1dbda92
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-26 11:06:48 +0600

    replace and remove practice done using DOM

--- add replace remove element using DOM.js
+++ add replace remove element using DOM.js
@@ -23,3 +23,36 @@ console.log(ulitem);
 
 //Replacing Elements 
 
+let newHeading = document.createElement("h1");
+newHeading.appendChild(document.createTextNode("H1 new Heading ")); 
+
+newHeading.className = "sample-class";
+
+let oldHeading  = document.querySelector("h3");
+
+let parent = document.querySelector(".container");
+//parent = oldHeading.parentElement;
+
+parent.replaceChild(newHeading,oldHeading);
+
+console.log(newHeading);
+console.log(oldHeading);
+console.log(parent);
+
+//Remove element
+let lis = document.querySelectorAll("li");
+let list = document.querySelector("ul");
+//lis[0].remove();
+//list.removeChild(lis[5]);
+
+list.classList.add("test");
+list.classList.add("test-new");
+list.classList.remove("test-new");
+let val = list.hasAttribute("title");
+list.setAttribute("title","yes");
+val = list.hasAttribute("title");
+list.removeAttribute("title");
+val = list.hasAttribute("title");
+console.log(val);
+
+
