c617c0ab50e824aeee9a48b9752890d22a501622
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-27 16:27:28 +0600

    Regular Expression practice start

---
+++ regular-expression.js
@@ -0,0 +1,21 @@
+//Regular expression
+//Pattern Matching Technique
+
+
+let re = /hello/i;
+
+let str  = "Hello World is revolbing";
+
+
+let result = re.test(str);//returns result in true or false
+
+result = re.exec(str);//returns result in an array for true or null value for false 
+result = str.match(re);// match is a string function opposite of excp but do same job
+result = str.search(re);// returns first index value where re is found 
+
+
+let newstr  = str.replace(re,"Hi"); // use to replace ina string 
+
+console.log(result);
+console.log(newstr);
+
