d35653fbf4833e7605b14641977726b48d84d763
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-28 21:25:59 +0600

    done home work Character Sets, Quantifiers and Groupings

--- literal and meta characters.js
+++ literal and meta characters.js
@@ -25,13 +25,37 @@ re = /hello?/; // escape everithing after question mark so {hello world} matches
 
 // Character Set Using Brackets []
 
-re = /h[eai]llo/; // must be one of them inside brackets and only one at atime
-re = /[ha]ello/; 
+re = /h[eai]llo/; // must be one of them inside brackets and only one at a time
+re = /[HA]ello/; // start with upper case H and A
+re = /[^ha]ello/; // Anything except those inside brackets 
+re = /^[ha]ello/; // Must start with h or a 
 
-str = "Hello world";
-str = "Again hllo World";
-str = "Hello";
-str = "haahaaahhaahaahhhahaaaahhhahahello world";
+re = /[A-Za-z]ello/; // range 
+re = /[0-9]ello/; // number range
+re = /[^0-9]ello/; // anything except digit at first
+re = /^[0-9]ello/; //must need to start with digit from 0 to 9
+
+re  = /[0-9][0-9][0-9]ello/;//must need to start with 3 digit from 0 to 9
+
+//Braces {} - Quantifier
+re = /el{1}lloo/; // l need to occur must at least 1 time after first e
+re = /el{0,3}/; // Range can use
+re = /el{2,}/; // at least 2 times
+
+//Parentheses () - Grouping 
+re = /[0-9]{3}/; ///[0-9][0-9][0-9]ello/
+
+re = /^([0-9]xy){4}/;
+
+//Bangladeshi phone number
+// total 11 digits
+re = /^01[0-9]{9}$/;
+re = /^\+8801[0-9]{9}$/;
+//str = "2xy1xy3xy3xy";
+str = "+8801788888888";
+
+
+
 reTest(re,str);
 function reTest(re,str){
     if(re.test(str)){
