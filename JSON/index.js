92592858832cd5de3b148a60e4e6c748c86a4e6b
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-30 23:56:40 +0600

    json does not support undefined function and date

--- JSON/index.js
+++ JSON/index.js
@@ -13,3 +13,22 @@ console.log(studentJson);
 var studentNew = JSON.parse(studentJson); // JSON object to JS object
 
 console.log(studentNew);
+
+// JSON does not support undefined ,date , function 
+
+var person = {
+    name : "Rahim",
+    age : 25,
+    hometown : "Dhaka",
+    married : false,
+    dob: 1995-05-12,
+    test_null:null,
+    test_undefined: undefined,
+    greet: function(){
+        console.log(`Hello ${this.name}`);
+    } 
+
+}
+
+var personJSON = JSON.stringify(person);
+console.log(personJSON);
