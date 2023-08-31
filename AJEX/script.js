3552e27cbab3afedf761c29ed7f06640bb2d7112
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-08-31 20:21:46 +0600

    AJAX intro done

--- AJEX/script.js
+++ AJEX/script.js
@@ -13,6 +13,34 @@ function loadData() {
     xhr.onprogress = function () {
         console.log(xhr.readyState);
     }
+    xhr.onload = function () {
+        // HTTP statuses
+        //200: "OK"
+        //403: "Forbidden"
+        //404: "Not Found"
+        if (this.status === 200) {
+            document.getElementById('output').innerHTML = `<h4>${this.responseText}</h4>`;
+        }
+    }
 
-    console.log(xhr);
+    // xhr.onreadystatechange = function () {
+    //     //readyState Values
+    //     // 0: request not initialized
+    //     // 1: server connection established
+    //     // 2: request received
+    //     // 3: processing request  
+    //     // 4: request finished and response is ready
+
+    //     if(this.status === 200 && this.readyState ===4){
+    //         console.log(this.responseText);
+
+    //     }
+
+
+    // }
+
+
+    xhr.send();
+
+    //console.log(xhr);
 }
