2c63db4e24f6376eca02b999070fecfb7f375c5e
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-03 17:44:16 +0600

    see you at home

--- Fetch API/script.js
+++ Fetch API/script.js
@@ -1,5 +1,53 @@
 //Callback function 
 
-setTimeout(function(){
-    console.log("Hello World!");
-},5000);
+// setTimeout(function(){
+//     console.log("Hello World!");
+// },5000);
+
+
+let persons = [
+    { firstName: "Simanta", lastName: "Paul" },
+    { firstName: "Fazle", lastName: "Rahat" }
+];
+
+
+// function createPerson(person) {
+//     setTimeout(function () {
+//         persons.push(person);
+//     }, 4000);
+// }
+
+// function getPerson() {
+//     setTimeout(function () {
+//         let output = '';
+//         persons.forEach(function (person){
+//             output += `<li>${person.firstName} ${person.lastName}</li>`
+//         });
+//         document.getElementById("output").innerHTML = output;
+//     },1000);
+// }
+
+// createPerson({firstName:"Rony", lastName:"Chy"});
+// getPerson();
+
+//Asyncrinous way 
+
+function createPerson(person,callback) {
+    setTimeout(function () {
+        persons.push(person);
+        callback();
+    }, 2000);
+}
+
+function getPerson() {
+    setTimeout(function () {
+        let output = '';
+        persons.forEach(function (person){
+            output += `<li>${person.firstName} ${person.lastName}</li>`
+        });
+        document.getElementById("output").innerHTML = output;
+    },1000);
+}
+
+createPerson({firstName:"Rony", lastName:"Chy"},getPerson);
+
