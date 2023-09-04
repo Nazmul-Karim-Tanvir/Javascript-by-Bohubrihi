72bdeaeb332fca0343d9ec628803e78ed760452b
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-04 12:03:27 +0600

    fetch api practice done

---
+++ Fetch API/fetch-api.js
@@ -0,0 +1,13 @@
+//Fetch API
+//Fetch API Uses JavaScript Promises
+
+
+document.getElementById("get_data").addEventListener("click",getData);
+
+
+function getData(){
+    fetch ('http://api.icndb.com/jokes/random')
+    .then(res => res.json)
+    .then(data => {console.log(data);})
+    .catch(err => {console.log(err);})
+}
