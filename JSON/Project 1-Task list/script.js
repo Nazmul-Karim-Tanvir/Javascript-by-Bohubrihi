e37b104032b43edd5920a88788317af338ca5ebb
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-04 13:17:48 +0600

    Continue after namaz

---
+++ JSON/Project 1-Task list/script.js
@@ -0,0 +1,7 @@
+let create_task = document.querySelector("create-task");
+let add_task = document.querySelector("add-task");
+let filter_task = document.querySelector("filter-task");
+let list_task = document.querySelector("list-task");
+let clear_task = document.querySelector("clear-task");
+
+
