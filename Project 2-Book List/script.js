82107a99435272389543a377856b178b026a0530
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-06 13:05:03 +0600

    will continue project2  after namaz

--- Project 2-Book List/script.js
+++ Project 2-Book List/script.js
@@ -1,7 +1,7 @@
 // Get the UI elements 
 
 let form = document.querySelector("#book-form");
-let booklist = document.querySelector("book-list");
+let booklist = document.querySelector("#book-list");
 
 
 
@@ -16,12 +16,9 @@ class Book {
 
 
 //UI class
-class UI{
-    constructor(){
+class UI {
 
-    }
-
-    addToBookList(book){
+    static addToBookList(book) {
         let list = document.querySelector('#book-list');
         let row = document.createElement('tr');
         row.innerHTML = `
@@ -33,30 +30,72 @@ class UI{
         list.appendChild(row);
     }
 
-    clearFields(){
-        let title = document.querySelector("#title").value = "",
-    author = document.querySelector("#author").value="",
-    isbn = document.querySelector("#isbn").value ="";
+    static clearFields() {
+        document.querySelector("#title").value = '';
+        document.querySelector("#author").value = '';
+        document.querySelector("#isbn").value = '';
     }
+    static deleteFromBook(target) {
+        if (target.hasAttribute("href")) {
+            target.parentElement.parentElement.remove();
+            UI.showAlert("Book Removed!", "success");
+        }
+
+    }
+
+    static showAlert(message, className) {
+        // creating div saction
+        let div = document.createElement("div");
+        div.className = `alert ${className}`;
+
+        //adding text node to div
+        let textNode = document.createTextNode(message);
+        div.appendChild(textNode);
+
+        //putting div brfore form in container saction
+        let container = document.querySelector(".container");
+        let form = document.querySelector("#book-form");
+        container.insertBefore(div, form);
+
+        // alert message will disappear after 3 seconds 
+        setTimeout(() => {
+            document.querySelector(".alert").remove();
+        }, 3000);
+
+    }
+
 }
 
 
 
 // Add event listener 
 form.addEventListener("submit", newBook);
+booklist.addEventListener("click", removeBook);
 
 
 // newBook 
 function newBook(e) {
     let title = document.querySelector("#title").value,
-    author = document.querySelector("#author").value,
-    isbn = document.querySelector("#isbn").value;
+        author = document.querySelector("#author").value,
+        isbn = document.querySelector("#isbn").value;
 
-    let book = new Book(title,author,isbn);
-    let ui = new UI();
 
-    ui.addToBookList(book);
-    ui.clearFields();
 
+    if (title === "" || author === "" || isbn === "") {
+        UI.showAlert("Please fill all the fields!", "error");
+    }
+    else {
+        let book = new Book(title, author, isbn);
+        UI.addToBookList(book);
+        UI.clearFields();
+        UI.showAlert("Book Added!", "success");
+    }
+    e.preventDefault();
+}
+
+// removeBook
+
+function removeBook(e) {
+    UI.deleteFromBook(e.target);
     e.preventDefault();
 }
