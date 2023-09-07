857ee401e55f3530ba50bb339a09a78f26389502
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-07 15:10:56 +0600

    project 3 done

--- Project 3-GitHub User Finder/script.js
+++ Project 3-GitHub User Finder/script.js
@@ -3,4 +3,26 @@ let searchUser = document.querySelector("#searchUser");
 
 let ui = new UI();
 
+searchBtn.addEventListener("click", (e) => {
+    let userText = searchUser.value;
+    if (userText != "") {
+        //Fetch API
+        fetch(`https://api.github.com/users/${userText}`)
+        .then(result => result.json())
+        .then(data =>{
+            if(data.message == "Not Found"){
+                //Show Alert 
+                ui.showAlert("User Not Found!","alert alert-danger");
+            }else{
+                //Show Profile
+                ui.showProfile();
+            }
+        })
+    }
+    else {
+        //Clear Profile
+        ui.clearProfile();
+    }
+});
+
 
