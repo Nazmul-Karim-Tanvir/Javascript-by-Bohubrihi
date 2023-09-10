bc772441a484d2c4fc88d7aff2a7e1dcdd139acc
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-10 15:55:41 +0600

    addToShoppingCart created

--- Project 4-Developing A Shopping Cart Using JavaScript/script.js
+++ Project 4-Developing A Shopping Cart Using JavaScript/script.js
@@ -1,13 +1,40 @@
-let viewshopingCart = document.querySelector("#view-shopping-cart");
-var cart = document.getElementById("shopping-cart");
-let camera = document.querySelector("#camera");
-let earphones = document.querySelector("#earphone");
-let laptop = document.querySelector("#laptop");
-let computer = document.querySelector("#computer");
+//product 
+let products = document.querySelectorAll("#product");
 
-// Add a click event listener to the button
+
+// shopping cart related 
+let viewshopingCart = document.querySelector("#view-shopping-cart");
+let shoppingCart = document.getElementById("shopping-cart");
+let shoppingCartItem = document.getElementById("shopping-cart-item");
+
+
+// Scroll to the shopping cart element
 viewshopingCart.addEventListener("click", function() {
-  // Scroll to the shopping cart element
-  cart.scrollIntoView();
+  shoppingCart.scrollIntoView();
 });
 
+//Add to cart will add item in Shopping Cart
+products.forEach(function(product){
+  product.addEventListener("click", addToShoppingCart);
+});
+
+
+function addToShoppingCart(e) {
+
+  //creating li element 
+  let li = document.createElement("li");
+  //getting text from camera sibling h5
+  let h5 = e.target.parentElement.childNodes[3].textContent;
+  let p1 = e.target.parentElement.childNodes[4].textContent;
+  let p2 = e.target.parentElement.childNodes[5].textContent;
+  //let text  = h5.textContent;
+
+  li.innerHTML = `Product detail: <i>${h5},${p1}, ${p2} </i> <button class= "badge text-bg-danger"> Remove  </button>`;
+  console.log(li);
+
+  shoppingCartItem.appendChild(li);
+
+
+  //preventing default behavior of click event
+  e.preventDefault();
+}
