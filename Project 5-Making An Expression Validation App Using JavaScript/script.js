42a43aec58980067f4a87b3d708a544af7a336ab
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-11 23:25:07 +0600

    project 5 done

--- Project 5-Making An Expression Validation App Using JavaScript/script.js
+++ Project 5-Making An Expression Validation App Using JavaScript/script.js
@@ -1,38 +1,28 @@
-// Get the elements from the HTML document
+// geting element from the html document 
 const dropdown = document.querySelector(".dropdown");
-const dropdownMenu = document.querySelector(".dropdown-menu");
 const dropdownButton = document.querySelector(".dropdown-toggle");
-const input = document.createElement("input"); // Create an input element
-const result = document.createElement("p"); // Create a paragraph element
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
-// Append the input and result elements to the dropdown element
+
+//creating a input and result saction under dropdown
+const input = document.createElement("input");
+const result = document.createElement("p");
+
 dropdown.appendChild(input);
 dropdown.appendChild(result);
 
-// Define the regular expressions for email, phone number and post code
+
+//defining the regular expression for email, phone number , postal code
+
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 const phoneRegex = /^\+?(\d{1,3})?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
-const postCodeRegex = /^[A-Z]{1,2}\d[A-Z\d]? ?\d[A-Z]{2}$/;
-
-// Define a function to validate the input value against the selected regular expression
-function validate(regex) {
-  // Get the input value
-  const value = input.value;
-  // Test if the value matches the regex
-  const isValid = regex.test(value);
-  // Display the result
-  result.textContent = isValid ? "Valid" : "Invalid";
-  // Change the result color
-  result.style.color = isValid ? "green" : "red";
-}
+const postCodeRegex = /^[0-9]{4}$/;
 
 // Add an event listener to the dropdown menu to handle the selection of validation type
 dropdownMenu.addEventListener("click", function (event) {
-  // Get the selected validation type
   const type = event.target.textContent;
-  // Update the dropdown button text
   dropdownButton.textContent = type;
-  // Set the input type and placeholder according to the validation type
+
   switch (type) {
     case "Email":
       input.type = "email";
@@ -49,16 +39,16 @@ dropdownMenu.addEventListener("click", function (event) {
     default:
       break;
   }
-  // Clear the input value and result text
   input.value = "";
   result.textContent = "";
 });
 
+
 // Add an event listener to the input element to handle the change of input value
+
 input.addEventListener("input", function () {
-  // Get the selected validation type
   const type = dropdownButton.textContent;
-  // Validate the input value according to the validation type
+
   switch (type) {
     case "Email":
       validate(emailRegex);
@@ -72,4 +62,24 @@ input.addEventListener("input", function () {
     default:
       break;
   }
+
 });
+
+
+//Regular expressin validate function 
+
+function validate(regex) {
+
+  const value = input.value;
+  const valid = regex.test(value);
+
+  if (valid === true) {
+    result.textContent = "Valid";
+    result.style.color = "green";
+  }
+  else {
+    result.textContent = "Invalid";
+    result.style.color = "red";
+  }
+
+}
