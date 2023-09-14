83f65375740fc0dcd4f6cdf75a717b68c42f674c
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-14 17:20:13 +0600

    function expression

---
+++ JavaScript Leveling up to ES6/arrow function.js
@@ -0,0 +1,20 @@
+// Normal Declaration
+function saySomething(name){
+    console.log("hello " + name);
+}
+
+saySomething("Bohubrihi");
+
+// Function Expression 
+let saySomething1 = function(name2){
+    console.log("hello "+name2);
+}
+saySomething1("Simanta");
+
+//  Arrow Function(ES6)
+
+let saySomething2 = (name3) =>{
+    console.log("I am "+name3);
+}
+
+saySomething2("JavaScript");
