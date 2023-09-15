4b9be59ed9b1ac34c27b863b047e3e9b97fdbb5d
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-16 00:27:28 +0600

    upload // Object Destructing

--- JavaScript Leveling up to ES6/object-distructing.js
+++ JavaScript Leveling up to ES6/object-distructing.js
@@ -5,12 +5,12 @@ let person = {
     lastName: "Rahat",
     dob: `09-27-1995`
 }
-let {firstName: fname, lastName: lname, dob} = person;
+// let {firstName: fname, lastName: lname, dob} = person;
 
-console.log(fname,lname,dob);
+// console.log(fname,lname,dob);
 
 function display({firstName: fname, lastName: lname, dob}){
-    console.log(firstName,lastName,dob);
+    console.log(fname,lname,dob);
 }
 
 display(person);
