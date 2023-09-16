063ad6979877e0dcd8f4bdcf287df32828d31cbb
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-16 20:44:32 +0600

    Symbols practice done

---
+++ JavaScript Leveling up to ES6/symbols es6 feature.js
@@ -0,0 +1,27 @@
+//Symbols
+let a = Symbol();
+// let b = Symbol();
+
+// console.log(a===b); // false 
+
+let person = {
+    name: "simanta",
+    age: 25,
+    [a]: "Hello World"
+}
+
+console.log(person);
+console.log(Object.getOwnPropertyNames(person));
+console.log(Object.getOwnPropertySymbols(person));
+console.log(Object.keys(person));
+console.log(JSON.stringify(person));
+
+//for cannot find symbol
+for (x in person){
+    console.log(x);
+} 
+
+let sym1 = Symbol("Hello");
+let sym2 = Symbol("Hello");
+
+console.log(sym1 == sym2); // false 
