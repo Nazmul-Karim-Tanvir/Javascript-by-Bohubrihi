48583b09b8cbfc25c821c8bb2b003149158251f9
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2023-09-21 13:52:42 +0600

    iterator and generator practice done

--- JavaScript Leveling up to ES6/iterators and genarators.js
+++ JavaScript Leveling up to ES6/iterators and genarators.js
@@ -1,45 +1,79 @@
-// // Iterator
-// let iterable = "Hello";
-// iterable = [1,2,3,4,5,5];
-// // Symbol.iterator
-// let iter = iterable[Symbol.iterator]();
+//Iterating over a string
 
-// console.log(iter);
-
-// console.log(iter.next());
-// console.log(iter.next());
-
-// console.log("Other Codes...");
-
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
-// console.log(iter.next());
+// const name1 = "W3Schools";
 
 
-// // Custom Iterator
-// function customIterator(arr) {
-//     let i = 0;
-
-//     return {
-//         next: function() {
-//             return i < arr.length ? { value: arr[i++], done: false} : { value: undefined, done: true };
-//         }
-//     };
+// for (const x of name1) {
+//     console.log(x);
 // }
 
-// let members = customIterator(names);
+// //Iterating over an Array
 
-// console.log(members.next().value);
-// console.log(members.next().value);
-// console.log(members.next().value);
-// console.log("Random codes...");
-// console.log(members.next());
-// console.log(members.next());
+// const letters = ["", "a", "b", "c"];
 
 
+// for (const x of letters) {
+//     console.log(x);
+// }
 
-// Generators
+// //Iterating over set
+
+// const letters1 = new Set([" ", "e", "f", "g"]);
+
+// for (const x of letters1) {
+//     console.log(x);
+// }
+
+// //Iterating over a map
+
+// const fruits = new Map([
+//     ["apples", 500],
+//     ["bananas", 300],
+//     ["oranges", 200]
+// ]);
+
+// for (const y of fruits) {
+//     console.log(y);
+// }
+
+// More on Iterator 
+
+let iterable = "Hello";
+
+let iter = iterable[Symbol.iterator]();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+
+// custom iterable 
+
+let iterable1 = [1, 2, 3, 4, 5, 5];
+
+function customIterator(arr) {
+    let i = 0;
+
+    return {
+        next: function() {
+            return i < arr.length ? 
+            { value: arr[i++], done: false} : { value: undefined, done: true };
+        }
+    };
+}
+
+let memmbers = customIterator(iterable1);
+
+console.log(memmbers.next());
+console.log(memmbers.next());
+console.log(memmbers.next());
+console.log(memmbers.next());
+console.log(memmbers.next());
+console.log(memmbers.next());
+console.log(memmbers.next());
+
+// Generators 
 
 function* genFunction() {
     console.log("I am some code");
@@ -54,7 +88,7 @@ function* genFunction() {
     yield "Hello World";
 }
 
-let iter = genFunction();
+let iter1 = genFunction();
 
-console.log(iter.next().value);
-console.log(iter.next().value);
+console.log(iter1.next().value);
+console.log(iter1.next().value);
